621eca39d72752c09200c90601affdf36a1b1577
Kurt <kstyers86@gmail.com>
2021-09-27 12:14:15 -0700

    Add check for whether or not to use a or an

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const data = require('./words.json');
+let message = [];
 
 const sentence = data.sentences[Math.ceil(
     Math.random() * Object.keys(data.sentences).length)];
 
 for (let i = 0; i < sentence.length; i++) {
     let wordNum = Math.floor(Math.random() * data[sentence[i]].length);
-    //if (sentence[i] === 'articles') {
-    //    let word = sentence[i + 1];
-    //    let letter = word[0];
-    //    console.log(letter);        
-    //}
-    // if word == article
-    // get first letter of next word
-    // if first letter is vowel randomly chooose an or the
-    // else randomly choose a or the
-
+    message.push(data[sentence[i]][wordNum]);
     
-    console.log(data[sentence[i]][wordNum]);
+    if (i != 0 && sentence[i-1] === "articles") {
+        let letter = message[i][0].toLowerCase();
+
+        if (message[i-1] === 'a' || message[i-1] === 'an') {
+            if (letter == 'a' || letter == 'e' || letter == 'i'
+            || letter == 'o' || letter == 'u') {
+                message[i-1] = 'an';
+            } else {
+                message[i-1] = 'a';
+            }
+        }
+        
+    }    
+     
 }
+
+console.log(message.join(" "));
