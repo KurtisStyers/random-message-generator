c46af36e876147f99d0672ef9ccfae8bbaf66dd7
Kurt <kstyers86@gmail.com>
2021-09-27 10:40:02 -0700

    Add for loop to randomly pick words for a sentence
    Added functionality to randomly select a sentence layout from the JSON file and a for loop to randomly pick the words.
    There is only one sentence layout so far. Working on functionality to pick a or an depending on the first letter of the word after the article.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const data = require('./test.json');
+const data = require('./words.json');
 
-console.log(data);
+const sentence = data.sentences[Math.ceil(
+    Math.random() * Object.keys(data.sentences).length)];
+
+for (let i = 0; i < sentence.length; i++) {
+    let wordNum = Math.floor(Math.random() * data[sentence[i]].length);
+    //if (sentence[i] === 'articles') {
+    //    let word = sentence[i + 1];
+    //    let letter = word[0];
+    //    console.log(letter);        
+    //}
+    // if word == article
+    // get first letter of next word
+    // if first letter is vowel randomly chooose an or the
+    // else randomly choose a or the
+
+    
+    console.log(data[sentence[i]][wordNum]);
+}
