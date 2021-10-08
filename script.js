ff996227988d6e36a1bcce62af4429f30b695ba3
Kurt <kstyers86@gmail.com>
2021-10-08 06:50:29 -0700

    Refactor for modularity

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const data = require('./words.json');
-let message = [];
 
-const sentence = data.sentences[Math.ceil(
-    Math.random() * Object.keys(data.sentences).length)];
+const randomMessage = () => {
+    let message = [];
 
-for (let i = 0; i < sentence.length; i++) {
-    let wordNum = Math.floor(Math.random() * data[sentence[i]].length);
-    message.push(data[sentence[i]][wordNum]);
-    
-    if (i != 0 && sentence[i-1] === "articles") {
-        let letter = message[i][0].toLowerCase();
+    // pick a random sentence structure from the sentences section of the JSON file
+    const sentence = data.sentences[Math.ceil(
+        Math.random() * Object.keys(data.sentences).length)];
 
-        if (message[i-1] === 'a' || message[i-1] === 'an') {
-            if (letter == 'a' || letter == 'e' || letter == 'i'
-            || letter == 'o' || letter == 'u') {
-                message[i-1] = 'an';
-            } else {
-                message[i-1] = 'a';
-            }
-        }        
-    }     
+    // loop through the different parts of the sentence and pick a random word for each
+    for (let i = 0; i < sentence.length; i++) {
+        let wordNum = Math.floor(Math.random() * data[sentence[i]].length);
+        message.push(data[sentence[i]][wordNum]);
+        
+        // pick a or an if the next word starts with a vowel
+        if (i != 0 && sentence[i-1] === "articles") {
+            let letter = message[i][0].toLowerCase();
+
+            if (message[i-1] === 'a' || message[i-1] === 'an') {
+                if (letter == 'a' || letter == 'e' || letter == 'i'
+                || letter == 'o' || letter == 'u') {
+                    message[i-1] = 'an';
+                } else {
+                    message[i-1] = 'a';
+                }
+            }        
+        }     
+    }
+    return message.join(" ");
 }
 
-console.log(message.join(" "));
+console.log(randomMessage());
